dff5770bfb87dd7ace5ec47552b8959a869f4d18
repoCodigo-so <gitjuanjo@gmail.com>
2023-04-19 23:09:38 -0600

    fix: infinite value block and goal reach

--- src/shorpath/ShortPath.java
+++ src/shorpath/ShortPath.java
@@ -18,6 +18,7 @@ public class ShortPath {
     private int cuenta;
     private int startX;
     private int startY;
+    private int disMax;
 
     public ShortPath() {
         frame = new JFrame("Short Path");
@@ -58,7 +59,7 @@ public class ShortPath {
                                 btn.setEnabled(false);
                                 
                             } else {
-                                btn.setValor(Integer.MAX_VALUE);
+                                btn.setValor(0);
                                 startX = row;
                                 startY = col;
                                 btn.setText("Inicio ");
@@ -70,8 +71,9 @@ public class ShortPath {
                             int confirm = JOptionPane.showConfirmDialog(frame, "¿Desea que esta sea la meta?", "Meta",
                                     JOptionPane.YES_NO_OPTION);
                             if (confirm == JOptionPane.YES_OPTION) {
+                                disMax = Math.abs(startX - row) + Math.abs(startY - col); // Distancia Manhattan
                                 btn.setGoal(true);
-                                btn.setValor(Integer.MAX_VALUE);
+                                btn.setValor(0);
                                 btn.setText("Meta");
                                 btn.setBackground(Color.orange);
                                 cuenta = 2;
@@ -94,13 +96,49 @@ public class ShortPath {
                 for (int i = 0; i < numRows; i++) {
                     for (int j = 0; j < numCols; j++) {
                         MyButton btn = buttons[i][j];
-                        if (btn.getValor() != Integer.MAX_VALUE){
-                            int weight = Math.abs(startX - i) + Math.abs(startY - j); // Distancia Manhattan
+                        MyButton btnTop = buttons[i][j];
+                        MyButton btnLeft = buttons[i][j];
+                        MyButton btnRight = buttons[i][j];
+                        MyButton btnButton = buttons[i][j];
+                        
+                        if(i != numRows-1){
+                            btnTop = buttons[i+1][j];
+                        }
+                        else if(i != 0){
+                            btnButton = buttons[i-1][j];
+                        }
+                        else if(j != 0){
+                            btnLeft = buttons[i][j-1];
+                        }
+                        else if(j != numCols-1){
+                            btnRight = buttons[i][j+1];
+                        }
+                        int weight = Math.abs(startX - i) + Math.abs(startY - j); // Distancia Manhattan
+                        if (btn.getValor() != Integer.MAX_VALUE ){
                             btn.setValor(weight);
                             if(weight+1 == cuenta){
-                                btn.setText("(" + i + "," + j + ") - Peso: " + btn.getValor());
-                                btn.setBackground(Color.yellow);
-                                
+                                if(weight == 1){
+                                    btn.setText("(" + i + "," + j + ") - Peso: " + btn.getValor());
+                                    btn.setBackground(Color.yellow);
+                                }
+                                else if(btn.isGoal() == true){
+                                    btn.setValor(0);
+                                    btn.setText("Meta");
+                                    btn.setBackground(Color.orange);
+                                    JOptionPane.showMessageDialog(null, "LLego a la meta.", "Alerta", JOptionPane.WARNING_MESSAGE);
+                                    cuenta = weight-1;
+                                }
+                                else{
+                                    if(disMax != btn.getValor()){
+                                        if(btnTop.getValor() == Integer.MAX_VALUE || btnButton.getValor() == Integer.MAX_VALUE || btnLeft.getValor() == Integer.MAX_VALUE || btnRight.getValor() == Integer.MAX_VALUE ){
+                                            btn.setValor(weight+1);
+                                            btn.setText("(" + i + "," + j + ") - Peso: " + btn.getValor());
+                                            btn.setBackground(Color.yellow);
+                                        }
+                                        btn.setText("(" + i + "," + j + ") - Peso: " + btn.getValor());
+                                        btn.setBackground(Color.yellow);
+                                        }
+                                }
                             }
                         }
                     }
@@ -118,12 +156,49 @@ public class ShortPath {
                 for (int i = 0; i < numRows; i++) {
                     for (int j = 0; j < numCols; j++) {
                         MyButton btn = buttons[i][j];
-                        if (btn.getValor() != Integer.MAX_VALUE){
-                            int weight = Math.abs(startX - i) + Math.abs(startY - j); // Distancia Manhattan
+                        MyButton btnTop = buttons[i][j];
+                        MyButton btnLeft = buttons[i][j];
+                        MyButton btnRight = buttons[i][j];
+                        MyButton btnButton = buttons[i][j];
+                        
+                        if(i != numRows-1){
+                            btnTop = buttons[i+1][j];
+                        }
+                        else if(i != 0){
+                            btnButton = buttons[i-1][j];
+                        }
+                        else if(j != 0){
+                            btnLeft = buttons[i][j-1];
+                        }
+                        else if(j != numCols-1){
+                            btnRight = buttons[i][j+1];
+                        }
+                        int weight = Math.abs(startX - i) + Math.abs(startY - j); // Distancia Manhattan
+                        if (btn.getValor() != Integer.MAX_VALUE ){
                             btn.setValor(weight);
-                            if(weight == cuenta-1){
-                                btn.setText("(" + i + "," + j + ") - Peso: " + btn.getValor());
-                                btn.setBackground(Color.cyan);
+                            if(weight+1 == cuenta){
+                                if(weight == 1){
+                                    btn.setText("(" + i + "," + j + ") - Peso: " + btn.getValor());
+                                    btn.setBackground(Color.cyan);
+                                }
+                                else if(btn.isGoal() == true){
+                                    btn.setValor(0);
+                                    btn.setText("Meta");
+                                    btn.setBackground(Color.orange);
+                                    JOptionPane.showMessageDialog(null, "LLego a la meta.", "Alerta", JOptionPane.WARNING_MESSAGE);
+                                    cuenta = weight-1;
+                                }
+                                else{
+                                    if(disMax != btn.getValor()){
+                                        if(btnTop.getValor() == Integer.MAX_VALUE || btnButton.getValor() == Integer.MAX_VALUE || btnLeft.getValor() == Integer.MAX_VALUE || btnRight.getValor() == Integer.MAX_VALUE ){
+                                            btn.setValor(weight+1);
+                                            btn.setText("(" + i + "," + j + ") - Peso: " + btn.getValor());
+                                            btn.setBackground(Color.cyan);
+                                        }
+                                        btn.setText("(" + i + "," + j + ") - Peso: " + btn.getValor());
+                                        btn.setBackground(Color.cyan);
+                                        }
+                                }
                             }
                         }
                     }
@@ -148,7 +223,7 @@ public class ShortPath {
             super("(" + row + "," + col + ")");
             this.row = row;
             this.col = col;
-            this.valor = 1;
+            this.valor = 0;
             this.goal = false;
             this.star = false;
             this.setText("(" + row + "," + col + ") - Valor: " + valor);
